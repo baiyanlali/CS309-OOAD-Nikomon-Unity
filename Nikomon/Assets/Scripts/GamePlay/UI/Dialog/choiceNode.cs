2902289d85db8fe03d0a72a03b0b13ae88f80a6c
Zhao Yunlong <zao44457660@126.com>
2021-10-04 15:34:07 +0800

    ？

---
+++ Nikomon/Assets/Scripts/GamePlay/UI/Dialog/choiceNode.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+class choiceNode
+{
+    public int choiceId;
+    public bool returnRelated;
+    public int dialogId;
+
+}
---
+++ Nikomon/Assets/Scripts/GamePlay/UI/Dialog/testMain.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class testMain : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public dialogEngine de;
+    private dialogManager dm;
+    private int condition;
+    void Start()
+    {
+        //de = transform.Find("dialogSystem").GetComponent<dialogEngine>();
+        dm = dialogManager.getDialogManager();
+        condition = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //dm.onDialogText("八百标兵奔北坡，炮兵并排北边跑！");
+        //dm.offDialog();
+        StartCoroutine("on");
+
+        
+
+    }
+    IEnumerator off()
+    {
+        
+        yield return new WaitForSeconds(2);
+        if (condition == 1)
+        {
+            Debug.Log("off");
+            dm.offDialog();
+            condition = 2;
+            
+        }
+
+    }
+    IEnumerator on()
+    {
+        yield return new WaitForSeconds(2);
+        if (condition == 0)
+        {
+            Debug.Log("on");
+            dm.onDialogText("八百标兵奔北坡，炮兵并排北边跑！");
+            condition = 1;
+            StartCoroutine("off");
+        }
+
+    }
+
+
+}
