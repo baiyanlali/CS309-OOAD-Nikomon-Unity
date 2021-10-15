134c4f00b0b6c66d91156730586d8f3d20de141f
Zhao Yunlong <zao44457660@126.com>
2021-10-15 18:23:06 +0800

    update

---
+++ Nikomon/Assets/Scripts/GamePlay/Core/Translator.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using PokemonCore.Utility;
+
+namespace GamePlay.Core
+{
+    public static class Translator
+    {
+        public static Dictionary<string, string> Translate;
+        public static int LanguageID;
+
+        public static void ChangeLanguage(int lang_id)
+        {
+            Translate = SaveLoad.Load<Dictionary<string,string>>("");
+        }
+
+        public static string TranslateStr(string str)
+        {
+            return null;
+        }
+    }
+}
