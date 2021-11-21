df8516759d9ad5479c26960362c714f2c5390df2
SUSTechkris <61013537+SUSTechkris@users.noreply.github.com>
2021-11-21 14:21:32 +0800

    1
    1

---
+++ Nikomon/Assets/Scripts/GamePlay/Character/NPC.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using PokemonCore.Combat;
+using UnityEngine;
+
+public class NPC : MonoBehaviour,IInteractive
+{
+    private Trainer _trainer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _trainer = new Trainer("WYF", true);
+        _trainer.party[0] = new Pokemon(1, 20);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnInteractive()
+    {
+        DialogHandler.Instance.InitBattle(null);
+        GlobalManager.Instance.StartBattle(null,new List<Trainer>(){_trainer},false,1);
+    }
+
+    public void OnInteractive(GameObject obj)
+    {
+        if (obj.name == "")
+        {
+            
+        }
+    }
+}
